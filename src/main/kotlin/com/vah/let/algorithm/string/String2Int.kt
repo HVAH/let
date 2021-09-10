a9a8d68f5d44b1f6262e3663bf3b8edece9f8d30
huangjiang <huangjiang@codemao.cn>
2021-09-10 11:00:50 +0800

    字符串转整数

--- src/main/kotlin/com/vah/let/algorithm/string/String2Int.kt
+++ src/main/kotlin/com/vah/let/algorithm/string/String2Int.kt
@@ -10,41 +10,49 @@ class String2Int {
 fun myAtoi(s: String): Int {
     if (s.isBlank()) return 0
     val str = s.trim()
-    var firstIndex = 0
+    var firstIndex = -1
     var lastIndex = 0
     var isFu = 0
-    val c1 = str[0]
-    if (c1 == '-') {
-        isFu = -1
-        firstIndex = 1
-    } else if (c1 == '+') {
-        firstIndex = 1
-    } else if ( c1 > '9' || c1 < '0') {
-        return 0
-    }
-    var firstNumIndex = 0
     for (i in str.indices) {
 
         val c = str[i]
         if (c in '0'..'9') {
-            if (firstIndex == 0) {
+            if (firstIndex == -1) {
+                if (i != 0 && str[i-1] == '-') {
+                    isFu = -1
+                }
                 firstIndex = i
             }
+            lastIndex = i
         } else if (c == '+' || c == '-') {
-
+            if (firstIndex != -1) {
+                break
+            }
+            if (i == str.length - 1) {
+                return 0
+            }
+            if (i != str.length - 1 && (str[i + 1] < '0' || str[i +1] > '9')) {
+                return 0
+            }
+        } else {
+            if (firstIndex == -1 && (c != '+' ||  c != '-')) {
+                return 0
+            }
+            if (firstIndex != -1) {
+                break
+            }
         }
-        lastIndex = i
     }
-    var toLong = str.substring(firstIndex + isFu, lastIndex + 1).toLong()
-    if (toLong < Int.MIN_VALUE) {
+    var toLong = str.substring(firstIndex + isFu, lastIndex + 1).toBigInteger()
+    if (toLong < Int.MIN_VALUE.toBigInteger()) {
        return Int.MIN_VALUE
     }
-    if (toLong > Int.MAX_VALUE) {
+    if (toLong > Int.MAX_VALUE.toBigInteger()) {
         return Int.MAX_VALUE
     }
     return toLong.toInt()
 }
 
 fun main() {
-    println(myAtoi("+-12"))
+    println(myAtoi("-5-"))
 }
