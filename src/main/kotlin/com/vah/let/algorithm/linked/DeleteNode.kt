35d6574a8eea3e96c2f9c9d35a26db83f7a99a32
huangjiang <huangjiang@codemao.cn>
2021-09-26 19:00:38 +0800

    删除链表中的节点

---
+++ src/main/kotlin/com/vah/let/algorithm/linked/DeleteNode.kt
@@ -0,0 +1,66 @@
+package com.vah.let.algorithm.linked
+
+
+/**
+ *@Description 删除链表中的节点
+ *@Author HuangJiang
+ **/
+
+
+class ListNode(var value: Int) {
+    var next: ListNode? = null
+}
+
+fun deleteNode(node: ListNode?) {
+    node?.value = node?.next?.value!!
+    node?.next = node.next!!.next
+}
+
+/**
+ * 功能描述: 删除链表的倒数第N个节点  两次遍历
+ * @Param:
+ * @Return:
+ */
+fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+    var temp: ListNode? = head
+    var size = 0
+    while (temp != null) {
+        size++
+        temp = temp?.next
+    }
+    val index = size - n + 1
+    temp = head
+    if (index == 1) {
+        return temp?.next
+    }
+    var count = 1
+    while (count != index - 1) {
+        temp = temp?.next
+        count++
+    }
+    temp?.next = temp?.next?.next
+    return head
+}
+
+/**
+ * 功能描述: 删除链表的倒数第N个节点  一次遍历
+ * @Param:
+ * @Return:
+ */
+fun removeNthFromEnd2(head: ListNode?, n: Int): ListNode? {
+    // 用一个list存储每一个节点 一次遍历完成后即可知道
+    val list = mutableListOf<ListNode?>()
+    var temp: ListNode? = head!!
+    list.add(temp)
+    while (temp?.next != null) {
+        temp = temp?.next
+        list.add(temp)
+    }
+    val index = list.size - n
+    if (index == 0) {
+        return head?.next
+    }
+    val listNode = list[index - 1]
+    listNode?.next = listNode?.next?.next
+    return head
+}
--- src/main/kotlin/com/vah/let/algorithm/sort/HeapSort.kt
+++ src/main/kotlin/com/vah/let/algorithm/sort/HeapSort.kt
@@ -14,9 +14,11 @@ fun main() {
 }
 
 fun heapSort(arr: IntArray) {
+    //1 构建大顶堆(一种完全二叉树 父节点大于等于其左右节点)
     for (i in arr.size / 2 - 1 downTo  0 ) {
         transformHeap(arr, i, arr.size)
     }
+    //2 调整交换
     for (i in arr.size - 1 downTo  1) {
         val t = arr[i]
         arr[i] = arr[0]
@@ -26,7 +28,7 @@ fun heapSort(arr: IntArray) {
 }
 
 /**
- * 功能描述: 转换成大顶堆(一种完全二叉树 父节点大于等于其左右节点)
+ * 功能描述: 转换堆
  * @Param: arr 数组
  * @Param: i 非叶子节点在数组中的索引
  * @Param: length 需要调整的数组的长度
@@ -43,7 +45,7 @@ fun transformHeap(arr: IntArray, i: Int, length: Int) {
         if (arr[k] > t) {
             arr[i] = arr[k]
             i = k
-        } else {
+        } else { // 从下至上所以可以直接退出
             break
         }
         k = k * 2 + 1 // 继续左节点
