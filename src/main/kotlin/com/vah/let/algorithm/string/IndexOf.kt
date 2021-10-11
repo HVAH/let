0253abde4a32df5fa2ad5ccb8eaa2aa3abb5796c
huangjiang <huangjiang@codemao.cn>
2021-10-11 17:37:12 +0800

    kmp算法

--- src/main/kotlin/com/vah/let/algorithm/string/IndexOf.kt
+++ src/main/kotlin/com/vah/let/algorithm/string/IndexOf.kt
@@ -7,6 +7,9 @@ package com.vah.let.algorithm.string;
 class IndexOf {
 }
 
+/**
+ * 暴力匹配
+ */
 fun indexOf(haystack: String, needle: String): Int {
     val length = needle.length
     val length1 = haystack.length
@@ -32,7 +35,7 @@ fun indexOf(haystack: String, needle: String): Int {
                 return -1
             }
             for (j in 0 until length / 2) {
-                if (haystack[j + i] != needle[j] || needle[length - j - 1] != haystack[i + (length - j - 1)]) {
+                if (needle[j] != haystack[j + i] || needle[length - j - 1] != haystack[(length - j - 1) + i]) {
                     contain = false
                     break
                 }
@@ -47,11 +50,86 @@ fun indexOf(haystack: String, needle: String): Int {
     return -1
 }
 
+fun kmpIndexOf(haystack: String, needle: String): Int {
+    if (haystack == needle || needle.isEmpty()) {
+        return 0
+    }
+    var j = 0
+    val next = getNext(needle)
+    for (i in haystack.indices) {
+        while (j > 0 && haystack[i] != needle[j]) {
+            j = next[j - 1]
+        }
+        if (haystack[i] == needle[j]) {
+            j++
+        }
+        if (j == needle.length) {
+            return i - j + 1
+        }
+    }
+    return -1
+}
+
+// 获取一个字符串的部分匹配值
+fun getNext(str: String): IntArray {
+    val r = IntArray(str.length)
+    r[0] = 0
+
+    var j = 0
+    for (i in 1 until str.length) {
+        while (j > 0 && str[i] != str[j]) {
+            j = r[j - 1]
+        }
+        if (str[i] == str[j]) {
+            j++
+        }
+        r[i] = j
+    }
+    return r
+}
+
+fun next(str: String): IntArray {
+    var i = 0
+    var j = -1
+    val next = IntArray(str.length)
+    next[0] = -1
+    while (i < str.length - 1) {
+        if (j == -1 || str[i] == str[j]) {
+            next[++i] = ++j
+        } else {
+            j = next[j]
+        }
+    }
+    return next
+}
+
+fun strStr(haystack: String, needle: String): Int {
+    if (needle.length == 0) return 0
+    var i = 0
+    var j = 0
+    val next = next(needle)
+    while (i < haystack.length && j < needle.length) {
+        if (j == -1 || haystack[i] == needle[j]) {
+            i++
+            j++
+        } else {
+            j = next[j]
+        }
+        if (j == needle.length) {
+            return i - j
+        }
+    }
+    return -1
+}
+
 fun main() {
-    println(
-        indexOf(
-            "mississippi",
-            "issip"
-        )
-    )
+//    val next = getNext("abcdabd")
+//    next.forEach { print(it) }
+//    val next2 = getNext2("abcdabd")
+//    println()
+//    next2.forEach { print(it) }
+
+//    println(kmpIndexOf("abc abcd abcdabcdabdcf", "abcdabd"))
+
+    next("abac").forEach { print(it) }
 }
